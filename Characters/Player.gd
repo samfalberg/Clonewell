b13626b0b773e3d4e1e7817dfa231029a5dadead
samfalberg <samuelfalberg@gmail.com>
2020-12-24 00:54:54 -0600

    Add enemy cat + movement

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -6,10 +6,12 @@ var gunboots_duration = 0
 
 # Add player movement and physics
 func _physics_process(delta: float):
+	# One bullet fired when gun button pressed once in air
 	if Input.is_action_just_pressed("jump_and_shoot") and not is_on_floor():
 		rounds_shot += 1
 		velocity.y = -500
 	
+	# Bullets fired continuously when gun button held down
 	if Input.is_action_pressed("jump_and_shoot") and not is_on_floor():
 		gunboots_duration += delta
 		if gunboots_duration >= .25:
@@ -37,7 +39,7 @@ func _physics_process(delta: float):
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump_and_shoot") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump_and_shoot") and is_on_floor() else 0.0
 	)
 
 # Return a Vector2 with the player's x and y velocity 
