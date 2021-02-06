63fa82c1ac166a2295c1e513149316f78c565df5
Sam Falberg <samuelfalberg@gmail.com>
2021-02-06 16:53:29 -0700

    Add restart button to pause menu

--- Interface/Pause.gd
+++ Interface/Pause.gd
@@ -10,7 +10,14 @@ func _input(event):
 func _on_ResumeButton_pressed():
 	var new_pause_state = not get_tree().paused 
 	get_tree().paused = new_pause_state
-	visible = new_pause_state # Menu is only visible when paused
+	visible = new_pause_state 
+
+# Restart game on restart button press
+func _on_RestartButton_pressed():
+	var new_pause_state = not get_tree().paused
+	get_tree().reload_current_scene()
+	get_tree().paused = new_pause_state
+	visible = new_pause_state 
 
 # Exit game on quit button press
 func _on_QuitButton_pressed():
