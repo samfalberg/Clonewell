acc1ad6fae024222de2fb1cc79de5ffe63e22e09
Sam Falberg <samuelfalberg@gmail.com>
2021-02-20 21:53:20 -0700

    Adjust effect of gravity

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -4,6 +4,6 @@ class_name Character
 const FLOOR_NORMAL = Vector2.UP
 
 export var speed = Vector2(800.0, 1400.0)
-export var gravity = 2500.0	
+export var gravity = 2000.0	
 
 var velocity = Vector2.ZERO
